6533274973b3e4fb43cd6d13ed880f42801392c6
Marta <160023329+Marta-fn@users.noreply.github.com>
2024-03-03 17:24:19 +0000

    Added the AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
-function generateQuote(event) {
-  event.preventDefault();
-
+function displayQuote(response) {
   new Typewriter("#quote", {
-    strings: "The quote will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 30,
     cursor: "",
   });
+}
+
+function generateQuote(event) {
+  event.preventDefault();
+  let userPrompt = document.querySelector("#user-prompt");
+  let apiKey = "8066f7209f55a447d5b43ta1a1a01obb";
+  let prompt = `Form a quote with or about the word ${userPrompt.value}`;
+  let context =
+    "You are an Assistant AI with a vast knowledge of positive quotes. Match the user prompt language to the quote";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let quoteFormElement = document.querySelector("#quote-generator");
