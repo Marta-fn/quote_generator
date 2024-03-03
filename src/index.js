f17944fe63acf39d9681abaa7fd345c2cb53ae9d
Marta <160023329+Marta-fn@users.noreply.github.com>
2024-03-03 21:14:56 +0000

    Improved the user Experience

--- src/index.js
+++ src/index.js
@@ -13,8 +13,12 @@ function generateQuote(event) {
   let apiKey = "8066f7209f55a447d5b43ta1a1a01obb";
   let prompt = `Form a quote with or about the word ${userPrompt.value}`;
   let context =
-    "You are an Assistant AI with a vast knowledge of positive quotes. Match the user prompt language to the quote";
+    "You are an Assistant AI with a vast knowledge of positive quotes. Match the quote to the user prompt language.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating"> Generating the perfect quote for you </div>`;
 
   axios.get(apiUrl).then(displayQuote);
 }
